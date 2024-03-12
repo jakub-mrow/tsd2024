5bbdd2c103dad38e8f79c2823f6d3e7a34c7093d
Jakub Mrówczyński <jakub@mrowczynski.net>
2024-03-12 19:53:05 +0100

    tasks 1

--- 03-LINQ/GoldSavings.App/Client/GoldClient.cs
+++ 03-LINQ/GoldSavings.App/Client/GoldClient.cs
@@ -50,4 +50,91 @@ public class GoldClient
 
     }
 
+    public async Task<List<GoldPrice>> GetGoldPricesLastYear()
+    {
+        DateTime endDate = DateTime.Now.Date;
+        DateTime startDate = endDate.AddYears(-1);
+
+        return await GetGoldPrices(startDate, endDate);
+    }
+
+    public async Task<List<GoldPrice>> GetPricesFromWholeYear(int year)
+    {
+        DateTime startDate = new DateTime(year, 01, 01);
+        DateTime endDate = new DateTime(year, 12, 31);
+
+        // create intervals that differ by 82 days
+        List<GoldPrice> prices = new List<GoldPrice>();
+        while (startDate < endDate)
+        {
+            DateTime nextDate = startDate.AddDays(82);
+            if (nextDate > endDate)
+            {
+                nextDate = endDate;
+            }
+            List<GoldPrice> intervalPrices = await GetGoldPrices(startDate, nextDate);
+            prices.AddRange(intervalPrices);
+            startDate = nextDate.AddDays(1);
+        }
+
+        return prices;
+    }
+
+    public async Task<List<GoldPrice>> GetPricesFromYears(int startYear, int endYear)
+    {
+        List<GoldPrice> prices = new List<GoldPrice>();
+        for (int year = startYear; year <= endYear; year++)
+        {
+            List<GoldPrice> yearPrices = await GetPricesFromWholeYear(year);
+            prices.AddRange(yearPrices);
+        }
+        return prices;
+    }
+
+    public async Task<double> CalculateAveragePrice(int year)
+    {
+        List<GoldPrice> prices = await GetPricesFromWholeYear(year);
+        double sum = 0;
+        foreach (var price in prices)
+        {
+            sum += price.Price;
+        }
+        return sum / prices.Count;
+    }
+
+    // When it would be best to buy gold and sell it between 2019 and 2023? What would be the return on investment?
+    public async Task<GoldPrice> GetBestTimeToBuyAndSell()
+    {
+        List<GoldPrice> prices = new List<GoldPrice>();
+        for (int year = 2019; year <= 2023; year++)
+        {
+            List<GoldPrice> yearPrices = await GetPricesFromWholeYear(year);
+            prices.AddRange(yearPrices);
+        }
+
+        GoldPrice buyPrice = prices[0];
+        GoldPrice sellPrice = prices[0];
+        double maxProfit = 0;
+        for (int i = 0; i < prices.Count; i++)
+        {
+            if (prices[i].Price < buyPrice.Price)
+            {
+                buyPrice = prices[i];
+            }
+            if (prices[i].Price > sellPrice.Price)
+            {
+                sellPrice = prices[i];
+            }
+            if (sellPrice.Price - buyPrice.Price > maxProfit)
+            {
+                maxProfit = sellPrice.Price - buyPrice.Price;
+            }
+        }
+        Console.WriteLine($"The best time to buy is {buyPrice.Date} and the best time to sell is {sellPrice.Date}");
+        Console.WriteLine($"The return on investment would be {maxProfit}: buy price {buyPrice.Price} and sell price {sellPrice.Price}");
+        return buyPrice;
+    }
+
+
+
 }
--- 03-LINQ/GoldSavings.App/Program.cs
+++ 03-LINQ/GoldSavings.App/Program.cs
@@ -14,10 +14,135 @@ class Program
         Console.WriteLine($"The price for today is {currentPrice.Price}");
 
         List<GoldPrice> thisMonthPrices = goldClient.GetGoldPrices(new DateTime(2024, 03, 01), new DateTime(2024, 03, 11)).GetAwaiter().GetResult();
-        foreach(var goldPrice in thisMonthPrices)
+        foreach (var goldPrice in thisMonthPrices)
         {
             Console.WriteLine($"The price for {goldPrice.Date} is {goldPrice.Price}");
         }
 
+        List<GoldPrice> lastYearPrices = goldClient.GetGoldPricesLastYear().GetAwaiter().GetResult();
+
+        // get three highest prices
+        var highestPrices = lastYearPrices.OrderByDescending(p => p.Price).Take(3);
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"The {i + 1} highest price is {highestPrices.ElementAt(i).Price}");
+        }
+        // get three highest prices with linq
+        var highestPricesLinq = (from p in lastYearPrices
+                                 orderby p.Price descending
+                                 select p).Take(3);
+
+        foreach (var goldPrice in highestPricesLinq)
+        {
+            Console.WriteLine($"The highest price is {goldPrice.Price}");
+        }
+
+        // get three lowest prices
+        var lowestPrices = lastYearPrices.OrderBy(p => p.Price).Take(3);
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"The {i + 1} lowest price is {lowestPrices.ElementAt(i).Price}");
+        }
+
+        // get three lowest prices with linq
+        var lowestPricesLinq = (from p in lastYearPrices
+                                orderby p.Price
+                                select p).Take(3);
+        foreach (var goldPrice in lowestPricesLinq)
+        {
+            Console.WriteLine($"The lowest price is {goldPrice.Price}");
+        }
+
+        // Task 4
+        // get all the values from 2020 january and check if comparing to 2020 01 01 the price is 1.05 time higher
+        Console.WriteLine("Task 4 -----------------------");
+
+        GoldPrice priceFirstJan2020 = goldClient.GetGoldPrices(new DateTime(2020, 01, 01), new DateTime(2020, 01, 02)).GetAwaiter().GetResult().First();
+
+        List<GoldPrice> january2020Prices = goldClient.GetGoldPrices(new DateTime(2020, 01, 01), new DateTime(2020, 01, 31)).GetAwaiter().GetResult();
+
+
+        foreach (GoldPrice price in january2020Prices)
+        {
+            if (price.Price >= priceFirstJan2020.Price * 1.05)
+            {
+                Console.WriteLine($"The price for {price.Date} is {price.Price} and is 1.05 times higher than the price for 2020, 01, 01");
+            }
+        }
+
+        // Task 5
+        
+        Console.WriteLine("Task 5 -----------------------");
+        List<GoldPrice> pricesTop = goldClient.GetPricesFromYears(2019, 2022).GetAwaiter().GetResult();
+
+        // sort descending by price
+        var sortedPricesTop = pricesTop.OrderByDescending(p => p.Price);
+
+        // skip 10 and take 3 from sortedPricesTop
+        var top3Prices = sortedPricesTop.Skip(10).Take(3);
+
+        // using linq
+        var top3PricesLinq = (from p in pricesTop
+                              orderby p.Price descending
+                              select p).Skip(10).Take(3);
+
+        foreach (var goldPrice in top3PricesLinq)
+        {
+            Console.WriteLine($"The price for {goldPrice.Date} is {goldPrice.Price}");
+        }
+
+        foreach (var goldPrice in top3Prices)
+        {
+            Console.WriteLine($"The price for {goldPrice.Date} is {goldPrice.Price}");
+        }
+
+
+        // Task 6
+        Console.WriteLine("Task 6 -----------------------");
+        List<GoldPrice> prices2020 = goldClient.GetPricesFromWholeYear(2021).GetAwaiter().GetResult();
+
+        // calculate average price from 2021 2022 2023
+        double averagePrice2021 = goldClient.CalculateAveragePrice(2021).GetAwaiter().GetResult();
+        double averagePrice2022 = goldClient.CalculateAveragePrice(2022).GetAwaiter().GetResult();
+        double averagePrice2023 = goldClient.CalculateAveragePrice(2023).GetAwaiter().GetResult();
+
+        Console.WriteLine($"The average price for 2021 is {averagePrice2021}");
+        Console.WriteLine($"The average price for 2022 is {averagePrice2022}");
+        Console.WriteLine($"The average price for 2023 is {averagePrice2023}");
+
+        // Task 7 
+        Console.WriteLine("Task 7 -----------------------");
+        GoldPrice bestTimeToBuyAndSell = goldClient.GetBestTimeToBuyAndSell().GetAwaiter().GetResult();
+
+
+        // save to file
+        savetoXML(lastYearPrices);
+
+        List<GoldPrice> newPrices = readFromXML();
+
+        // foreach(var price in newPrices)
+        // {
+        //     Console.WriteLine($"The price for {price.Date} is {price.Price}");
+        // }
+
+    
+
+    }
+    static void savetoXML(List<GoldPrice> prices)
+    {
+        var xml = new System.Xml.Serialization.XmlSerializer(typeof(List<GoldPrice>));
+        using (var stream = new System.IO.FileStream("prices.xml", System.IO.FileMode.Create))
+        {
+            xml.Serialize(stream, prices);
+        }
+    }
+
+    static List<GoldPrice> readFromXML()
+    {
+        var xml = new System.Xml.Serialization.XmlSerializer(typeof(List<GoldPrice>));
+        using (var stream = new System.IO.FileStream("prices.xml", System.IO.FileMode.Open))
+        {
+            return (List<GoldPrice>)xml.Deserialize(stream);
+        }
     }
 }
